4379dbbdc0620b1bd1f1a244a5103f20fd37e4d5
stormyff97 <stormyff97@gmail.com>
2025-12-17 02:37:30 +0100

    Update PythonPlayer.cpp
    Fix autopickupstatus

--- client_src/UserInterface/PythonPlayer.cpp
+++ client_src/UserInterface/PythonPlayer.cpp
@@ -9,3 +9,12 @@ bool CPythonPlayer::HasPetAutoPickup() const
 	return m_bPetAutoPickup;
 }
 #endif
+
+//into--> void CPythonPlayer::Clear()
+	//search:
+	m_inGuildAreaID = 0xffffffff;
+
+//add after:
+#ifdef PET_AUTODROP
+	m_bPetAutoPickup = false;
+#endif
